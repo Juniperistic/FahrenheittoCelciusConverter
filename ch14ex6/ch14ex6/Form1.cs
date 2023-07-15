99592463ac8e5979133b494c9472cb4a371d42b4
Miranda Morris <94880693+Juniperistic@users.noreply.github.com>
2023-07-15 19:35:12 -0400

    Upload of Fahrenheit to Celcius Converter

---
+++ ch14ex6/ch14ex6/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch14ex6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            double con, f;
+            f = Convert.ToDouble(textBox1.Text);
+            con = (((f - 32)) * 5 / 9);
+            label2.Text = con.ToString();
+
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ch14ex6/ch14ex6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*************************************************************************/
+/* Program Name:     Ch14Ex6.cs                                          */
+/* Date:             03/22/2022                                     	 */
+/* Programmer:       Miranda Morris                                 	 */
+/* Class:            CSCI 234                                            */
+/*                                                    				     */
+/* Program Description: The purpose of this program is to convert a temp */
+/* that is in Fahrenheit to Celsius.                                     */
+/*                                                                       */
+/* Input: a temp in Fahrenheit									         */
+/*                                                                       */
+/* Output: the same inputted temp converted to Celsius                   */
+/*                                                                       */
+/* Givens: Celsius = (5/9) * (Fahrenheit - 32)						     */
+/*                                                                       */
+/* Testing Data:                                                         */
+/*                                                                       */
+/* List the Testing Input Data: 50,                                      */
+/*                              90,                                      */
+/*                              70,                                      */
+/*                                                                       */
+/* List the Testing Output Data: 10,                                     */
+/*                               32.22,                                  */
+/*                               21.11                                   */
+/*                                                                       */
+/*************************************************************************/
+
+namespace ch14ex6
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
